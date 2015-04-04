d76f0e3e69d1b4331c40331e5abeceb782109a68
Kwag De Gi <code@skyaperture.com>
2015-04-04 12:48:05 +0900

    entity manager setting done

--- src/main/java/net/javacafe/hibernate/Helloworld.java
+++ src/main/java/net/javacafe/hibernate/Helloworld.java
@@ -1,6 +1,11 @@
 package net.javacafe.hibernate;
 
-import java.util.List;
+import java.util.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import net.javacafe.hibernate.domain.AnnotatedMessage;
 import net.javacafe.hibernate.domain.Message;
@@ -12,48 +17,40 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class Helloworld {
-	private static SessionFactory sessionFactory = new Configuration()
-			.configure().buildSessionFactory();
-
-	private static SessionFactory annotatedSessionFactory = new AnnotationConfiguration()
-			.addPackage("net.javacafe.hibernate.domain")
-			.addAnnotatedClass(AnnotatedMessage.class).configure()
-			.buildSessionFactory();
 
 	public static void main(String[] args) {
-		Session session = sessionFactory.openSession();
-		Transaction tx = session.beginTransaction();
-
-		Message message = new Message("Hello World");
-		session.save(message);
-
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("helloworld");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		Message message = new Message("Hello World2");
+		em.persist(message);
+		
 		tx.commit();
-		session.close();
-
-		Session secondSession = annotatedSessionFactory.openSession();
-		Transaction secondTx = secondSession.beginTransaction();
-
-		AnnotatedMessage annotatedMessage = new AnnotatedMessage("Hello World");
-		secondSession.save(annotatedMessage);
-
-		secondTx.commit();
-		secondSession.close();
-
-		Session newSession = sessionFactory.openSession();
-		Transaction newTransaction = newSession.beginTransaction();
-		List messages = newSession.createQuery(
-				"from Message m order by m.text asc").list();
-
-		System.out.println(messages.size() + " message(s) found:");
-
+		em.close();
+		
+		EntityManager newEm = emf.createEntityManager();
+		EntityTransaction newTx = newEm.getTransaction();
+		newTx.begin();
+		
+		List messages = newEm
+				.createQuery("select m from Message m order by m.text asc")
+				.getResultList();
+		
+		System.out.println(messages.size() + " message(s) foooooound");
+		
 		for (Object m : messages) {
 			Message loadedMsg = (Message) m;
 			System.out.println(loadedMsg.getText());
 		}
-
-		newTransaction.commit();
-		newSession.close();
-
-		sessionFactory.close();
+		
+		newTx.commit();
+		newEm.close();
+		
+		emf.close();
+			
 	}
+		
 }
